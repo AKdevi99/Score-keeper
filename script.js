691352d9484897e69e279ec98ec0bf5773e53317
Aditya singh <aditya.rpsk@gmail.com>
2024-09-11 16:29:08 +0530

    score change logic

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-const score = document.querySelector(".score");
-const noplayers = document.querySelector("#playingto");
+const scorediv = document.querySelector(".score");
+let scoretext = scorediv.textContent;
+let score = scoretext.split("to").map(s=>parseInt(s.trim(),10));
+
+const limit = document.querySelector("#playingto");
 const player1 =  document.querySelector(".player1");
 const player2 = document.querySelector(".player2");
-const reset = document.querySelector("reset");
+const reset = document.querySelector(".reset");
+
+
+
+
+player1.addEventListener("click",function()
+{
+    console.log(score);
+    scorediv.innerHTML = `<b>${score[0]} to ${score[1]}</b>`;
+});
+
+player2.addEventListener("click", function() {
+    console.log(score);
+    scorediv.innerHTML = `<b>${score[0]} to ${score[1]}</b>`
+}) ;
+
+reset.addEventListener("click",function()
+{
+    console.log(score);
+    scorediv.innerHTML = `<b>${score[0]} to ${score[1]}</b>`
+}) ;
 
