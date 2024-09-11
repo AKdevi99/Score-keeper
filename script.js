520bf219d7155f2e20d18814f5ed32d5aa6b5e99
Aditya singh <133839771+AKdevi99@users.noreply.github.com>
2024-09-11 15:52:24 +0530

    day one commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const score = document.querySelector(".score");
+const noplayers = document.querySelector("#playingto");
+const player1 =  document.querySelector(".player1");
+const player2 = document.querySelector(".player2");
+const reset = document.querySelector("reset");
+
